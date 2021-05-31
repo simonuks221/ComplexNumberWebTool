e10f952d1c1c1476febc710a3f29462e77e9f2a0
simonuks221 <simonasriauka@gmail.com>
2021-05-31 18:01:15 +0300

    Complex logic exported to seperate js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header"
 import ComplexNumbers from "./components/ComplexNumbers"
 
-import {AddComplex, SubtractComplex} from './ComplexNumberLogic'
+import {AddComplex, SubtractComplex, PartitionComplex, ReconstructComplex} from './ComplexNumberLogic'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -23,48 +23,32 @@ const deleteNumber  = (id) => {
 
 //ChangeComplexNUmberVlaue
 const changeNumber = (newNumber, id, numberType) =>{
-  var regExRec = /^(?:(?<real>\d+(?:(?:\.\d+)?(?:e[+\-]\d+)?)?)?(?:[+\-]))?(?<imaginary>\d+(?:(?:\.\d+)?(?:e[+\-]\d+)?)?)?[iI]$/i
-  var regExExp = /^(?:(?<amplitude>\d+(?:(?:(?:\.\d+)?)))(?:e))(?<phase>(?:[+\-])?(\d+(?:\.\d+)?)?)?[iI]$/i
-  var regExTime = /^(?:(?<amplitude>\d+(?:(?:(?:\.\d+)?)))(?:cos\())(?:(?<frequency>\d+(?:)(?:(?:\.\d+)?)))(?<phase>(?:[+\-])?(\d+(?:\.\d+)?)?)?(?:(\)))$/i
-  var result
+  var result = PartitionComplex(newNumber)
   switch(numberType){
     default:
       break;
     case 0:
-      result = newNumber.match(regExRec)
       if (result === null){
         setNumbers(complexNumbers.map((number) => (number.id === id ?{... number, rectangularForm: newNumber, exponentialForm: '', timeForm: '', correct: false} : number)))
       }
       else{
-        var newAmplitude = Math.sqrt(Math.pow(result[1], 2) + Math.pow(result[2], 2))
-        var newPhase = Math.atan(result[2]/result[1]) * 180 / Math.PI
-        var newExponentialForm = newAmplitude.toFixed(3) + 'e' + newPhase.toFixed(3) + 'i'
-        var newTimeForm = newAmplitude.toFixed(3) + 'cos(w' + (newPhase > 0 ? '+': '') + newPhase.toFixed(3) + ')'
-        setNumbers(complexNumbers.map((number) => (number.id === id ?{... number, rectangularForm: newNumber, exponentialForm: newExponentialForm, timeForm: newTimeForm, correct: true} : number)))
-
+        setNumbers(complexNumbers.map((number) => (number.id === id ?{... number, rectangularForm: newNumber, exponentialForm: ReconstructComplex(result, 'exponential'), timeForm: ReconstructComplex(result, 'time'), correct: true} : number)))
       }
-
       break;
     case 1:
-      result = newNumber.match(regExExp)
       if (result === null){
         setNumbers(complexNumbers.map((number) => (number.id === id ?{... number, rectangularForm: '', exponentialForm: newNumber, timeForm: '', correct: false} : number)))
       }
       else{
-        var newRectangularForm = result[1] * Math.cos(result[2] * Math.PI / 180).toFixed(3) + (result[1] > 0 ? '+': '') + result[1] * Math.sin(result[2] * Math.PI / 180).toFixed(3) + 'i'
-        var newTimeForm = result[1] + 'cos(w' + (result[2] > 0 ? '+': '-') + result[2] + ')'
-        setNumbers(complexNumbers.map((number) => (number.id === id ?{... number, rectangularForm: newRectangularForm, exponentialForm: newNumber, timeForm: newTimeForm, correct: true} : number)))
+        setNumbers(complexNumbers.map((number) => (number.id === id ?{... number, rectangularForm: ReconstructComplex(result, 'rectangular'), exponentialForm: newNumber, timeForm: ReconstructComplex(result, 'time'), correct: true} : number)))
       }
       break;
     case 2:
-      result = newNumber.match(regExTime)
       if (result === null){
         setNumbers(complexNumbers.map((number) => (number.id === id ?{... number, rectangularForm: '', exponentialForm: '', timeForm: newNumber, correct: false} : number)))
       }
       else{
-        var newRectangularForm = result[1] * Math.cos(result[3] * Math.PI / 180).toFixed(3) + (newAmplitude > 0 ? '+': '') + result[1] * Math.sin(result[3] * Math.PI / 180).toFixed(3) + 'i'
-        var newExponentialForm = result[1] + 'e' + result[3] + 'i'
-        setNumbers(complexNumbers.map((number) => (number.id === id ?{... number, rectangularForm: newRectangularForm, exponentialForm: newExponentialForm, timeForm: newNumber, correct: true} : number)))
+        setNumbers(complexNumbers.map((number) => (number.id === id ?{... number, rectangularForm: ReconstructComplex(result, 'rectangular'), exponentialForm: ReconstructComplex(result, 'exponential'), timeForm: newNumber, correct: true} : number)))
       }
       break;
   }
--- src/ComplexNumberLogic.js
+++ src/ComplexNumberLogic.js
@@ -1,13 +1,61 @@
 //Gets real and imaginary parts of rectangular form
-const PartitionComplex = (number) => {
+export const PartitionComplex = (number) => {
     var regExRec = /^(?:(?<real>\d+(?:(?:\.\d+)?(?:e[+\-]\d+)?)?)?(?:[+\-]))?(?<imaginary>\d+(?:(?:\.\d+)?(?:e[+\-]\d+)?)?)?[iI]$/i
+    var regExExp = /^(?:(?<amplitude>\d+(?:(?:(?:\.\d+)?)))(?:e))(?<phase>(?:[+\-])?(\d+(?:\.\d+)?)?)?[iI]$/i
+    var regExTime = /^(?:(?<amplitude>\d+(?:(?:(?:\.\d+)?)))(?:cos\())(?:(?<frequency>\d+(?:)(?:(?:\.\d+)?)))(?<phase>(?:[+\-])?(\d+(?:\.\d+)?)?)?(?:(\)))$/i
+
     var result = number.match(regExRec)
-    return [result[1], result[2]]
+    try{
+        if(result.length < 3){//Rectangular form
+            result = number.match(regExExp)
+
+            if(result.length < 3){//Exponential form
+                result = number.match(regExTime)
+
+                if(result.length < 3){ //Time form
+                    console.log('Error reading string')
+                    return null
+                }
+                else{
+                    var newReal = result[1] * Math.cos(result[3] * Math.PI / 180).toFixed(3)
+                    var newImag = result[1] * Math.sin(result[3] * Math.PI / 180).toFixed(3)
+                    return [parseInt(newReal), parseInt(newImag)]
+                }
+            }
+            else{ //Expoenntial
+                var newReal = result[1] * Math.cos(result[2] * Math.PI / 180).toFixed(3)
+                var newImag = result[1] * Math.sin(result[2] * Math.PI / 180).toFixed(3)
+                return[newReal, newImag]
+            }
+        }
+        else{ //Rectangular
+            return [parseInt(result[1]), parseInt(result[2])]
+        }
+    } 
+    catch(error){
+        console.log('Error reading string')
+        return null
+    }
 }
 
 //Reconstructs complex number from real and imag parts to rectangular form
-const ReconstructComplex = (realPart, imagPart) => {
-    return realPart + (realPart > 0? '+': '-') + imagPart + 'i'
+export const ReconstructComplex = (parts, form) => { //Parts[0] - real, 1 - imag
+    var newAmplitude = Math.sqrt(Math.pow(parts[0], 2) + Math.pow(parts[1], 2))
+    var newPhase = Math.atan(parts[1]/parts[0]) * 180 / Math.PI
+    switch(form){
+        default:
+            return parts[0] + (parts[0] > 0? '+': '-') + parts[1] + 'i'
+            break;
+        case 'rectangular':
+            return parts[0] + (parts[0] > 0? '+': '-') + parts[1] + 'i'
+            break;
+        case 'exponential':
+            return newAmplitude.toFixed(3) + 'e' + newPhase.toFixed(3) + 'i'
+            break;
+        case 'time':
+            return newAmplitude.toFixed(3) + 'cos(w' + (newPhase > 0 ? '+': '') + newPhase.toFixed(3) + ')'
+            break;
+    }
 }
 
 export const AddComplex = (number1, number2) => {
