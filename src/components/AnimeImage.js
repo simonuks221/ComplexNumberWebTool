496c569b2cd91e5ad7818b185fba8aa01ba92b35
simonuks221 <simonasriauka@gmail.com>
2021-06-29 12:52:56 +0300

    Updates

--- src/components/AnimeImage.js
+++
@@ -1,9 +0,0 @@
-const AnimeImage = ({animeImageInfo}) => {
-    return (
-        <div className = 'container my-5'>
-            <img src = {animeImageInfo.url} className = 'img rounded mx-auto d-block' style = {{height: "35rem" }} alt = 'catgirls for the wiiiin'/>
-        </div>
-    )
-}
-
-export default AnimeImage
